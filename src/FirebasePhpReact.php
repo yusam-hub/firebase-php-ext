943c56bed5a7904513803ca12d72ef20ce7d23e4
yusam <yusam@yandex.ru>
2023-07-14 01:43:02 +0300

    #react

--- src/FirebasePhpReact.php
+++ src/FirebasePhpReact.php
@@ -21,73 +21,90 @@ class FirebasePhpReact
         $this->serviceAccountModel = $serviceAccountModel;
     }
 
+    /**
+     * @param string $body
+     * @param callable $onResponse
+     * @param callable $onFail
+     * @return void
+     */
+    protected function fcmMessagesSend(string $body, callable $onResponse, callable $onFail): void
+    {
+        $browser = new Browser();
+
+        /**
+         * todo: нужна блокировка, что бы только один поток обратился за токеном, а остальные ждали когда появиться токен
+         */
+        if (AuthTokenModel::Instance()->isExpired()) {
+            //если нет токена, получаем его и потом запрос к firebase
+            $browser
+                ->request('POST', $this->serviceAccountModel->getTokenUrl(),
+                    [
+                        'Cache-Control' => 'no-store',
+                        'Content-Type' => 'application/x-www-form-urlencoded',
+                    ],
+                    Query::build([
+                        'grant_type' => $this->serviceAccountModel::JWT_URN,
+                        'assertion' => $this->serviceAccountModel->toJwt()
+                    ])
+                )
+                ->then(
+                    function (ResponseInterface $response) use ($browser, $body, $onResponse, $onFail) {
+
+                        if ($response->getStatusCode() !== 200) {
+                            $exception = new \RuntimeException("Invalid response of getting access token request");
+                            $onFail($exception);//пока такой вариант, надо узнать как в promise обрабатывается исключения типа throw new \Exception
+                            return;
+                        }
+
+                        AuthTokenModel::Instance()->assign((array)json_decode((string)$response->getBody(), true));
+
+                        //запрос к firebase
+                        $browser
+                            ->request('POST', $this->serviceAccountModel->getFcmProjectsMessagesSendUrl(),
+                                [
+                                    'Content-Type' => 'application/json',
+                                    'Authorization' => AuthTokenModel::Instance()->getAuthorizationHeaderValue(),
+                                ],
+                                $body
+                            )
+                            ->then(
+                                $onResponse,
+                                $onFail
+                            );
+                    },
+                    $onFail
+                );
+        } else {
+            //если есть токен, сразу запрос к firebase
+            $browser
+                ->request('POST', $this->serviceAccountModel->getFcmProjectsMessagesSendUrl(),
+                    [
+                        'Content-Type' => 'application/json',
+                        'Authorization' => AuthTokenModel::Instance()->getAuthorizationHeaderValue(),
+                    ],
+                    $body
+                )
+                ->then(
+                    $onResponse,
+                    $onFail
+                );
+        }
+    }
+
     public function testReactPhp(LoopInterface $loop): void
     {
-        $message = [
-            'data' => [
-                'title' => 'title test account',
-                'body' => 'body test account',
-                'icon' => 'https://localhost',
-                'image' => 'https://localhost',
-                'click_action' => 'https://localhost',
-                'actions' => json_encode([
-                    [
-                        'title' => 'buttonTitle1',
-                        'action' => 'button1',
-                    ],
-                    [
-                        'title' => 'buttonTitle2',
-                        'action' => 'button2',
-                    ],
-                ]),
-            ]
-        ];
+        $fcmBodyRequest = '{"message":{"data":{"title":"title test account","body":"body test account","icon":"https:\/\/localhost","image":"https:\/\/localhost","click_action":"https:\/\/localhost","actions":"[{\"title\":\"buttonTitle1\",\"action\":\"button1\"},{\"title\":\"buttonTitle2\",\"action\":\"button2\"}]"},"token":"dr8ZnHpbucuAvkXavpjZxd:APA91bFV2ljpYg3Jwa_MWlmRqloGJVqgJEAZn8LYebmcWUkhFRbiRtD9pkfFYmASTwFPL3eyZqrCTOOqFtEc6nTUHEIn8RBMVqMXp88pO-Y4E2pbtIyNFVu4uIqrD3JGvV4gaAsLIZIT"},"validate_only":false}';
 
-        $browser = new Browser();
-        $browser
-            ->request('POST', $this->serviceAccountModel->getTokenUrl(),
-            [
-                'Cache-Control' => 'no-store',
-                'Content-Type' => 'application/x-www-form-urlencoded',
-            ],
-            Query::build([
-                'grant_type' => $this->serviceAccountModel::JWT_URN,
-                'assertion' => $this->serviceAccountModel->toJwt()
-            ])
-        )
-        ->then(
-            function (ResponseInterface $response) use ($loop, $browser) {
+        $this->fcmMessagesSend($fcmBodyRequest,
+            function(ResponseInterface $response) use($loop) {
                 if ($response->getStatusCode() !== 200) {
                     $loop->stop();
                 }
-
-                AuthTokenModel::Instance()->assign((array)json_decode((string)$response->getBody(), true));
-                var_dump(AuthTokenModel::Instance()->toArray());
-
-                $browser
-                    ->request('POST', $this->serviceAccountModel->getFcmProjectsMessagesSendUrl(),
-                        [
-                            'Content-Type' => 'application/json',
-                            'Authorization' => AuthTokenModel::Instance()->getAuthorizationHeaderValue(),
-                        ],
-                        '{"message":{"data":{"title":"title test account","body":"body test account","icon":"https:\/\/localhost","image":"https:\/\/localhost","click_action":"https:\/\/localhost","actions":"[{\"title\":\"buttonTitle1\",\"action\":\"button1\"},{\"title\":\"buttonTitle2\",\"action\":\"button2\"}]"},"token":"dr8ZnHpbucuAvkXavpjZxd:APA91bFV2ljpYg3Jwa_MWlmRqloGJVqgJEAZn8LYebmcWUkhFRbiRtD9pkfFYmASTwFPL3eyZqrCTOOqFtEc6nTUHEIn8RBMVqMXp88pO-Y4E2pbtIyNFVu4uIqrD3JGvV4gaAsLIZIT"},"validate_only":false}'
-                    )
-                    ->then(
-                        function (ResponseInterface $response) use ($loop, $browser) {
-                            if ($response->getStatusCode() !== 200) {
-                                $loop->stop();
-                            }
-                            $res = (array)json_decode((string)$response->getBody(), true);
-                            var_dump($res);
-                            $loop->stop();
-                        },
-                        function (\Exception $e) use ($loop) {
-                            var_dump($e->getMessage());
-                            $loop->stop();
-                        }
-                    );
+                $res = (array)json_decode((string)$response->getBody(), true);
+                var_dump($res);
+                $loop->stop();
             },
-            function (\Exception $e) use ($loop) {
+            function(\Exception $e) use($loop) {
                 var_dump($e->getMessage());
                 $loop->stop();
             }
