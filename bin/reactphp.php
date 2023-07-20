a78cb9a9b29670b4c157986b4e2dc98a9a98b79e
yusam <yusam@yandex.ru>
2023-07-20 14:41:53 +0300

    #fix

--- bin/reactphp.php
+++ bin/reactphp.php
@@ -1,6 +1,8 @@
 <?php
 
 use GuzzleHttp\Psr7\Query;
+use Psr\Http\Message\ResponseInterface;
+use React\Http\Message\ResponseException;
 use YusamHub\FirebasePhpExt\FirebasePhpReact;
 
 require_once(__DIR__ . "/../vendor/autoload.php");
@@ -15,7 +17,30 @@ $timer = $loop->addPeriodicTimer(0.1, function ($timer) use ($loop) {
     $config = require __DIR__ . '/../config/config.php';
 
     $firebasePhpReact = new FirebasePhpReact(new \YusamHub\FirebasePhpExt\Fcm\ServiceAccountModel($config));
-    $firebasePhpReact->testReactPhp_fcmMessagesSend($loop);
+
+    $fcmBodyRequestValidate = '{"message":{"data":{"title":"title test account","body":"body test account","icon":"https:\/\/localhost","image":"https:\/\/localhost","click_action":"https:\/\/localhost","actions":"[{\"title\":\"buttonTitle1\",\"action\":\"button1\"},{\"title\":\"buttonTitle2\",\"action\":\"button2\"}]"},"token":"dr8ZnHpbucuAvkXavpjZxd:APA91bFV2ljpYg3Jwa_MWlmRqloGJVqgJEAZn8LYebmcWUkhFRbiRtD9pkfFYmASTwFPL3eyZqrCTOOqFtEc6nTUHEIn8RBMVqMXp88pO-Y4E2pbtIyNFVu4uIqrD3JGvV4gaAsLIZIT"},"validate_only":false}';
+    //$fcmBodyRequestValidate = '{"message":{"data":{"title":"title test account","body":"body test account","icon":"https:\/\/localhost","image":"https:\/\/localhost","click_action":"https:\/\/localhost","actions":"[{\"title\":\"buttonTitle1\",\"action\":\"button1\"},{\"title\":\"buttonTitle2\",\"action\":\"button2\"}]"},"token":"dr8ZnHpbucuAvkXavpjZxd:APA91bFV2ljpYg3Jwa_MWlmRqloGJVqgJEAZn8LYebmcWUkhFRbiRtD9pkfFYmASTwFPL3eyZqrCTOOqFtEc6nTUHEIn8RBMVqMXp88pO-Y4E2pbtIyNFVu4uIqrD3JGvV4gaAsLIZIT"},"validate_only":true}';
+    //$fcmBodyRequestValidate = '{"message":{"token":"dr8ZnHpbucuAvkXavpjZxd:APA91bFV2ljpYg3Jwa_MWlmRqloGJVqgJEAZn8LYebmcWUkhFRbiRtD9pkfFYmASTwFPL3eyZqrCTOOqFtEc6nTUHEIn8RBMVqMXp88pO-Y4E2pbtIyNFVu4uIqrD3JGvV4gaAsLIZIT"},"validate_only":true}';
+    //$fcmBodyRequestValidate = '{"message":{"token":"dr8ZnHpbucuAvkXavpjZxd:1APA91bFV2ljpYg3Jwa_MWlmRqloGJVqgJEAZn8LYebmcWUkhFRbiRtD9pkfFYmASTwFPL3eyZqrCTOOqFtEc6nTUHEIn8RBMVqMXp88pO-Y4E2pbtIyNFVu4uIqrD3JGvV4gaAsLIZIT"},"validate_only":true}';
+
+    $firebasePhpReact->fcmMessagesSend($fcmBodyRequestValidate,
+        function (ResponseInterface $response) use ($loop) {
+            if ($response->getStatusCode() !== 200) {
+                $loop->stop();
+            }
+            $res = (array)json_decode((string)$response->getBody(), true);
+            var_dump($res);
+            $loop->stop();
+        },
+        function (ResponseException $e) use ($loop) {
+            if (FirebasePhpReact::isRegistrationTokenInvalid($e)) {
+                var_dump("Invalid registration token");
+            } else {
+                var_dump("Error: " . $e->getMessage());
+            }
+            $loop->stop();
+        }
+    );
 
 });
 
--- src/FirebasePhpReact.php
+++ src/FirebasePhpReact.php
@@ -29,7 +29,7 @@ class FirebasePhpReact
      * @param callable $onFail
      * @return void
      */
-    protected function fcmMessagesSend(string $body, callable $onResponse, callable $onFail): void
+    public function fcmMessagesSend(string $body, callable $onResponse, callable $onFail): void
     {
         $browser = new Browser();
 
@@ -50,7 +50,7 @@ class FirebasePhpReact
                     function (ResponseInterface $response) use ($browser, $body, $onResponse, $onFail) {
 
                         if ($response->getStatusCode() !== 200) {
-                            $exception = new \RuntimeException("Invalid response of getting access token request");
+                            $exception = new ResponseException($response, "Invalid response of getting access token request");
                             $onFail($exception);//пока такой вариант, надо узнать как в promise обрабатывается исключения типа throw new \Exception
                             return;
                         }
@@ -90,34 +90,11 @@ class FirebasePhpReact
         }
     }
 
-    public function testReactPhp_fcmMessagesSend(LoopInterface $loop): void
-    {
-        $fcmBodyRequestValidateFalse = '{"message":{"data":{"title":"title test account","body":"body test account","icon":"https:\/\/localhost","image":"https:\/\/localhost","click_action":"https:\/\/localhost","actions":"[{\"title\":\"buttonTitle1\",\"action\":\"button1\"},{\"title\":\"buttonTitle2\",\"action\":\"button2\"}]"},"token":"dr8ZnHpbucuAvkXavpjZxd:APA91bFV2ljpYg3Jwa_MWlmRqloGJVqgJEAZn8LYebmcWUkhFRbiRtD9pkfFYmASTwFPL3eyZqrCTOOqFtEc6nTUHEIn8RBMVqMXp88pO-Y4E2pbtIyNFVu4uIqrD3JGvV4gaAsLIZIT"},"validate_only":false}';
-        $fcmBodyRequestValidateTrue = '{"message":{"data":{"title":"title test account","body":"body test account","icon":"https:\/\/localhost","image":"https:\/\/localhost","click_action":"https:\/\/localhost","actions":"[{\"title\":\"buttonTitle1\",\"action\":\"button1\"},{\"title\":\"buttonTitle2\",\"action\":\"button2\"}]"},"token":"dr8ZnHpbucuAvkXavpjZxd:APA91bFV2ljpYg3Jwa_MWlmRqloGJVqgJEAZn8LYebmcWUkhFRbiRtD9pkfFYmASTwFPL3eyZqrCTOOqFtEc6nTUHEIn8RBMVqMXp88pO-Y4E2pbtIyNFVu4uIqrD3JGvV4gaAsLIZIT"},"validate_only":true}';
-        $fcmBodyRequestValidateTrueWithoutBody = '{"message":{"token":"dr8ZnHpbucuAvkXavpjZxd:APA91bFV2ljpYg3Jwa_MWlmRqloGJVqgJEAZn8LYebmcWUkhFRbiRtD9pkfFYmASTwFPL3eyZqrCTOOqFtEc6nTUHEIn8RBMVqMXp88pO-Y4E2pbtIyNFVu4uIqrD3JGvV4gaAsLIZIT"},"validate_only":true}';
-        $fcmBodyRequestValidateTrueWithoutBodyInvalidToken = '{"message":{"token":"dr8ZnHpbucuAvkXavpjZxd:1APA91bFV2ljpYg3Jwa_MWlmRqloGJVqgJEAZn8LYebmcWUkhFRbiRtD9pkfFYmASTwFPL3eyZqrCTOOqFtEc6nTUHEIn8RBMVqMXp88pO-Y4E2pbtIyNFVu4uIqrD3JGvV4gaAsLIZIT"},"validate_only":true}';
-
-        $this->fcmMessagesSend($fcmBodyRequestValidateTrueWithoutBodyInvalidToken,
-            function (ResponseInterface $response) use ($loop) {
-                if ($response->getStatusCode() !== 200) {
-                    $loop->stop();
-                }
-                $res = (array)json_decode((string)$response->getBody(), true);
-                var_dump($res);
-                $loop->stop();
-            },
-            function (ResponseException $e) use ($loop) {
-                if ($this->isRegistrationTokenInvalid($e)) {
-                    var_dump("Invalid registration token");
-                } else {
-                    var_dump("Error: " . $e->getMessage());
-                }
-                $loop->stop();
-            }
-        );
-    }
-
-    protected function isRegistrationTokenInvalid(ResponseException $e): bool
+    /**
+     * @param ResponseException $e
+     * @return bool
+     */
+    public static function isRegistrationTokenInvalid(ResponseException $e): bool
     {
         $res = (array)json_decode((string)$e->getResponse()->getBody(), true);
         return ($e->getCode() === 400) && (isset($res['error']['message']) && mb_strstr($res['error']['message'], self::ERROR_STRING_INVALID_TOKEN));
